0e4b2212a00771a0a8d0b75f66b7e3d541eb6633
ZeroTrace7 <shreyashgupta999@gmail.com>
2026-04-15 21:38:51 +0530

    Youtube Playback Fixxxing

--- modules/background.js
+++ modules/background.js
@@ -89,7 +89,6 @@ function buildYouTubeEmbedUrl(videoId) {
     fs: '0',
     iv_load_policy: '3',
     loop: '1',
-    modestbranding: '1',
     playsinline: '1',
     playlist: videoId,
     rel: '0',
